f22af789d2e154e043033508e9612783499c4055
Kao Saelor <saelor.kao@gmail.com>
2023-08-23 17:57:00 -0700

    setup file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import React, { useEffet } from 'react';
 import { 
-  BrowserRouter, 
-  // Routes, 
-  // Route 
-} from 'react-router-dom';
+  BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
 import './App.css';
 
@@ -28,13 +28,56 @@ function App() {
           </div>
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              SideBar
+              <Sidebar />
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
-              Sidebar w-0
+              <Sidebar />
             </div>
           )}
+          <div
+            className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+          </div>
+          {themeSettings && (<ThemeSettings />)}
+
+          <div>
+            <Routes>
+              {/* DASHBOARD */}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+
+              {/* PAGES */}
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employee" element={<Employees />} />
+              <Route path="/customer" element={<Customers />} />
+
+              {/* APPS */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              {/* CHARTS */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramind" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+
+            </Routes>
+          </div>
+<Footer />
         </div>
       </BrowserRouter>
     </div>
