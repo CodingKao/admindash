369881706bd3372f6262375498db26e4e9182700
Kao Saelor <saelor.kao@gmail.com>
2023-09-11 23:25:29 -0700

    theme options

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -6,19 +6,24 @@ import { earningData, SparklineAreaData, ecomPieChartDat } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
+
+  const { currentColor } = useStateContext();
+
   return (
-      <div className="mt-12">
+      <div className="mt-2">
         <div className="flex flex-wrap lg:flex-nowrap justify-center">
-          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repat bg-cover bg-center">
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
             <div className="flex justify-between items-center">
-              <p className="font-bold text-gray-400">Earnings</p>
-              <p className="text-2xl">$63,448.78</p>
+              <div>
+                <p className="font-bold text-gray-400">Earnings</p>
+                <p className="text-2xl">$63,448.78</p>
+              </div>
             </div>
-          </div>
+            
           <div className="mt-6">
             <Button 
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRaduis="10px"
               size="md"
@@ -53,6 +58,7 @@ const Ecommerce = () => {
               <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
+          </div>
         </div>
 
         <div className="fle gap-10 flex-wrap justify-center">
@@ -83,23 +89,31 @@ const Ecommerce = () => {
                   </p>
                   <p className="text-gray mt-1">Budget</p>
                 </div>
-                <div className="mt-8">
-                  <p>
-                    <span className="text=3xl font-semibold">$48,438</span>
-                  </p>
-                  <p className="text-gray mt-1">Expense</p>
-                </div>
-                <div className="mt-5">
-                  <SparkLine 
-                    currentColor="blue"
-                    id="line-sparkline"
-                    type="Line"
-                    height="80px"
-                    width="250px"
-                    data={SparklineAreaData}
-                    color="blue"
-                  /> 
-                </div>
+                  <div className="mt-8">
+                    <p>
+                      <span className="text=3xl font-semibold">$48,438</span>
+                    </p>
+                    <p className="text-gray mt-1">Expense</p>
+                  </div>
+                    <div className="mt-5">
+                      <SparkLine 
+                        currentColor="blue"
+                        id="line-sparkline"
+                        type="Line"
+                        height="80px"
+                        width="250px"
+                        data={SparklineAreaData}
+                        color="blue"
+                      /> 
+                    </div>
+                    <div className="mt-10">
+                    <Button
+                      color="white"
+                      bgColor={currentColor}
+                      text="Download Report"
+                      borderRadius="10px"
+                    />
+                  </div>
               </div>
               <div>
                 <Stacked width="320px"
